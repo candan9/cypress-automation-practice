bca7b9e486109476c19eeb248da18824b2d1a261
Emre Candan <49526501+candan9@users.noreply.github.com>
2022-06-16 00:49:02 +0300

    fix bugs

--- cypress.config.js
+++ cypress.config.js
@@ -4,25 +4,19 @@ module.exports = defineConfig({
   pageLoadTimeout: 30000,
   env: {
     signIn: {
-      email: "teqqqs2su3eetmailforvirgosol@gmail.com",
+      email: "teqqqs2su3asdeetmailforvirgosol@gmail.com",
       password: "Ee1234012400.",
     },
   },
-  screenshotsFolder: false,
+  screenshotsFolder:false,
+  screenshotOnRunFailure:false,
   video: false,
   projectId: "i8o7aa",
   e2e: {
-    baseUrl: "https://automationpractice.com/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://automationpractice.com",
   },
   reporter: "cypress-multi-reporters",
   reporterOptions: {
     configFile: "reporter-config.json",
-  },
-  reporterEnabled: "spec, mocha-junit-reporter",
-  mochaJunitReporterReporterOptions: {
-    mochaFile: "cypress/results/junit/results-[hash].xml",
-  },
+  }
 });
